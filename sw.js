2d6ebdf3aa9a6eb86f461c28451e92aa520cea0c
FrancaTM <francatm@outlook.com>
2019-10-02 16:25:53 -0300

    implement service worker add sample content for offline work

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-console.log("hello");
+const cacheName = "cache-v1";
+const resourcesToPrecache = [
+  "/",
+  "index.html",
+  "styles/main.css",
+  "images/sample.jpg"
+];
+
+self.addEventListener("install", event => {
+  console.log("service worker install event");
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(resourcesToPrecache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  console.log("activate event");
+});
+
+self.addEventListener("fetch", event => {
+  console.log("fetch intercepted for: ", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
